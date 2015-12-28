4f771b1ea17d3f1c819fdb88248ac4faa147740a
DarrellBrogdon <dbrogdon@monigle.com>
2015-12-27 18:04:51 -0700

    Initial checkin

--- Mindfulness Bell/ViewController.swift
+++ Mindfulness Bell/ViewController.swift
@@ -7,12 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    let BellTypeSmall = 0
+    let BellTypeLarge = 1
+    
+    @IBOutlet var intervalTextField: UITextField!
+    @IBOutlet var intervalTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet var bellTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet var startStopButton: UIButton!
+    
+    var theIntervalType = 1
+    var theBellType = 0
+    
+    var audioPlayer: AVAudioPlayer!
+    var smallBellSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("small", ofType: "wav")!)
+    var largeBellSound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("large", ofType: "wav")!)
+    
+    @IBAction func setIntervalType(sender: UISegmentedControl) {
+        theIntervalType = sender.selectedSegmentIndex
+        print("Setting interval type to: \(theIntervalType)");
+    }
+    
+    @IBAction func setBellType(sender: UISegmentedControl) {
+        theBellType = sender.selectedSegmentIndex
+        print("Setting the bell type to: \(theBellType)")
+    }
+    
+    @IBAction func toggleBell(sender: UIButton) {
+//        if startStopButton.titleLabel!.text == "Start" {
+//            startStopButton.setTitle("Stop", forState: .Selected)
+//        } else {
+//            startStopButton.setTitle("Start", forState: .Selected)
+        
+            do {
+                switch theBellType {
+                case 1:
+                    self.audioPlayer = try AVAudioPlayer(contentsOfURL: smallBellSound)
+                default:
+                    self.audioPlayer = try AVAudioPlayer(contentsOfURL: largeBellSound)
+                }
+                
+                self.playBell()
+            } catch {
+                print("Error getting the audio file")
+            }
+//        }
+    }
+    
+    func playBell() {
+        self.audioPlayer.play()
+        print("Playing the bell")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        intervalTypeSegmentedControl.selectedSegmentIndex = 1
     }
 
     override func didReceiveMemoryWarning() {
